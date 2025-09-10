8e32e9ed66ec20e67236f8b49a7cbbc003e66c1d
Aien Pahlevan <aienpahlevanhim@gmail.com>
2025-09-10 17:20:01 +0330

    Install JWT package and create token handler class

---
+++ src/Utils/TokenHandler.cs
@@ -0,0 +1,61 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using BadeHava.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BadeHava.Utils;
+
+public static class TokenHandler
+{
+    public static string GenerateRefreshToken()
+    {
+        var randomBytes = RandomNumberGenerator.GetBytes(64);
+        return Convert.ToBase64String(randomBytes);
+    }
+
+    public static string GenerateAccessToken(User user, string secret, int expireMinutes = 15)
+    {
+        var securityTokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(secret);
+
+        var tokenDescription = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+            }),
+            Expires = DateTime.UtcNow.AddMinutes(expireMinutes),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = securityTokenHandler.CreateToken(tokenDescription);
+        return securityTokenHandler.WriteToken(token);
+    }
+
+    public static ClaimsPrincipal? ValidateAccessToken(string token, string secret)
+    {
+        var securityTokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(secret);
+
+        try
+        {
+            var principal = securityTokenHandler.ValidateToken(token, new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuerSigningKey = true
+            }, out SecurityToken validatedToken);
+
+            return principal;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
